4bb869c8abb77217149a82d561c862e58dd85f18
Vadim <viznjk.1994@gmail.com>
2025-05-10 12:55:54 -0500

    create user_update test

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+const supertest = require('supertest');
+
+global.request = supertest('http://localhost:8001/api/v1');
+global.TextEncoder = require('util').TextEncoder;
+global.TextDecoder = require('util').TextDecoder;
